d792ebb571ece026a7bf73c81e0afb25d887524c
Damien <boenapfel.damien@yahoo.fr>
2024-11-21 22:24:05 +0100

    maj

--- exo4/script.js
+++ exo4/script.js
@@ -3,18 +3,22 @@ const facebook = document.querySelector(".facebook");
 const twitter = document.querySelector(".twitter");
 const instagram = document.querySelector(".instagram");
 
-function Changement(color) {
-    document.body.style.background = color;  //  COULEUR DE BODY !
+function Changements(element, color, name) { // CLASS(qui contient mes elements) + COULEUR + SPAN
+    document.body.style.background = color;  //  CHANGE COULEUR DE BODY ! (background)
+
+    const socialNames = element.querySelector(".social-name");
+    socialNames.textContent = name; // MAJ TEXTE 
+    element.classList.add("clicked"); // Interrupteur
 }
 
 facebook.addEventListener("click", function() {
-    Changement("rgb(1, 101, 225)");
+    Changements(facebook, "rgb(1, 101, 225)", "Facebook");
 });
 
 twitter.addEventListener("click", function() {
-    Changement("rgb(29, 155, 240)");
+    Changements(twitter, "rgb(29, 155, 240)", "Twitter");
 });
 
 instagram.addEventListener("click", function() {
-    Changement("rgb(193, 53, 132)");
+    Changements(instagram, "rgb(193, 53, 132)", "Instagram");
 });
