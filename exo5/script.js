c80e8afe967e86d676028c46bc84fb576f329fcd
Damien <boenapfel.damien@yahoo.fr>
2024-11-22 16:45:39 +0100

    maj

--- exo5/script.js
+++ exo5/script.js
@@ -9,14 +9,24 @@ const board = document.getElementById("board");
 
 quotes.forEach(function(i) {
     console.log(i);
-    const rectangle = document.createElement("div");
+    const rectangle = document.createElement("div"); // CREE DIV RECTANGLE
     rectangle.classList.add("rectangle")
 
-    const title = document.createElement("h2")
+    const title = document.createElement("p") // TITRE
     title.classList.add("quote-title");
     title.textContent = i.title;
 
-    rectangle.appendChild(title);
+    const author = document.createElement("p") // AUTEUR
+    author.classList.add("quote-author");
+    author.textContent = i.author;
 
-    board.appendChild(rectangle);
+    const content = document.createElement("p") // CONTENU
+    content.classList.add("quote-content");
+    content.textContent = i.content;
+
+    rectangle.appendChild(title); // ALL DANS RECTANGLE
+    rectangle.appendChild(author);
+    rectangle.appendChild(content);
+
+    board.appendChild(rectangle); // RECTANGLE DANS BOARD
 })
