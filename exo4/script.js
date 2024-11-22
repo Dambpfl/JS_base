5c3f6bb92cb2182c5c2b135064a838c465fccd83
Damien <boenapfel.damien@yahoo.fr>
2024-11-22 13:48:37 +0100

    maj

--- exo4/script.js
+++ exo4/script.js
@@ -3,13 +3,21 @@ const facebook = document.querySelector(".facebook");
 const twitter = document.querySelector(".twitter");
 const instagram = document.querySelector(".instagram");
 
+const BodyColorInitial = window.getComputedStyle(document.body).backgroundColor;
+
 function Changements(element, color, name) { // CLASS(qui contient mes elements) + COULEUR + SPAN
-    document.body.style.background = color;  //  CHANGE COULEUR DE BODY ! (background)
+    const couleurBase = document.body.style.backgroundColor;      // couleur de base = body
+
+    if(couleurBase === color) {       // Si la couleur de base est  strictement egale à la couleur (icone)
+        document.body.style.backgroundColor = BodyColorInitial; // alors couleur body
+    } else {
+        document.body.style.backgroundColor = color; // sinon couleur icone
+    }
 
     const socialNames = element.querySelector(".social-name");
     socialNames.textContent = name; // MAJ TEXTE 
-    element.classList.add("clicked"); // Interrupteur
 }
+
 
 facebook.addEventListener("click", function() {
     Changements(facebook, "rgb(1, 101, 225)", "Facebook");
